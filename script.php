2c10f91cc302abab9eab97486240011df7528b25
Megumi Mochizuki <mochizuki@rista.jp>
2017-06-17 23:33:44 +0900

    インデント揃えた

--- script.php
+++ script.php
@@ -6,24 +6,24 @@
 <body>
 
 <?php
-	$rss = simplexml_load_file('http://d.hatena.ne.jp/neket/rss');
+  $rss = simplexml_load_file('http://d.hatena.ne.jp/neket/rss');
   echo '<ul>';
 
   foreach ($rss->item as $item) {
-		$link = $item ->link;
-		$title = $item ->title;
+    $link = $item ->link;
+    $title = $item ->title;
 
-		date_default_timezone_set('Asia/Tokyo');
-		$dc = $item ->children('http://purl.org/dc/elements/1.1/');
+    date_default_timezone_set('Asia/Tokyo');
+    $dc = $item ->children('http://purl.org/dc/elements/1.1/');
     $day = date('Y/m/d',strtotime($dc -> date));
-  ?>
+?>
 
-	<li>
-		<span class="date"><?php echo $day; ?></span>
+  <li>
+    <span class="date"><?php echo $day; ?></span>
     <a href="<?php echo $link; ?>">
-		    <span class="title"><?php echo $title; ?></span>
-		</a>
-	</li>
+      <span class="title"><?php echo $title; ?></span>
+    </a>
+  </li>
 <?php }  echo '</ul>'; ?>
 
 </body>
